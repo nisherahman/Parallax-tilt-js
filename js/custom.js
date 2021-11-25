b8517ccf2235b0a04d9862993c813492ca0359ed
unknown <you@example.com>
2021-11-25 19:45:31 +0600

    file save

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    //paralax background
+    $('#banner').parallax({
+        imageSrc: 'images/banner.jpg'
+    });
+
+    //tilt js
+    const tilt = $('.js-tilt').tilt();
+
+});
